0239ec10b9921942dda1b7dd8b06a9115c5e4cff
rzkyalbani <rzkyalbani@gmail.com>
2025-09-15 23:51:32 +0700

    fix: change 'name' to 'username' in fillable var

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -11,7 +11,7 @@ class Admin extends Authenticatable
     use Notifiable;
 
     protected $fillable = [
-        'name', 'email', 'password',
+        'username', 'email', 'password',
     ];
 
     protected $hidden = [
